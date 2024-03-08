342ac3244c2f5054cb6473559ad4e485adf1a5da
Florian <fwchartier@gmail.com>
2024-03-08 12:12:39 +0100

    too much add to describe

--- public/partials/final-countdown-public-display.php
+++ public/partials/final-countdown-public-display.php
@@ -16,13 +16,27 @@
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
 <?php
  
-$result = getAnnouncementInProgress();
+$result = getAnnouncementInProgress(true);
+
 foreach ($result as $row) {
+    if ($row->file_name) {
 ?>
-    <div data-id="<?= $row->id_headband ?>" style="background-color:<?= $row->bg_color ?>" class="update-nag notice notice-success inline hb_container">
+        <div data-id="<?= $row->id_headband ?>" style="background-image:url('http://wp-plugin.local/wp-content/plugins/final-countdown/admin/uploads/<?= $row->file_name ?>');height:<?= $row->height ?>px" class="update-nag notice notice-success inline hb_container">
+<?php
+    } else {
+?>
+        <div data-id="<?= $row->id_headband ?>" style="background-color:<?= $row->bg_color ?>;height:<?= $row->height ?>px" class="update-nag notice notice-success inline hb_container">
+<?php
+    };
+    if (intval($row->display_text) === 1) {
+?>
         <h2 style="color:<?= $row->title_color ?>" class="hb_ttl"><?= $row->title ?></h2>
         <h3 style="color:<?= $row->text_color ?>" class="hb_text"><?= $row->text ?></h3>
         <p>Du <?= $row->start_timer ?> au <?= $row->end_timer ?>.</p>
-    </div>
+<?php        
+    };
+?>
+        </div>
+
 <?php
 };
